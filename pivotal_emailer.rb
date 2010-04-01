5be949b66971ca98a9a3bd1dba42b2d1f018f374
Jason Noble <perlwizard@gmail.com>
2010-04-01 09:26:55 -0400

    [JN/EP] [#2999753] Auto Email stake holders about finished stories
    - Moved email subject into config
    - Added inactive user handling
      - Email address and subject in config
      - Different email preamble

--- pivotal_emailer.rb
+++ pivotal_emailer.rb
@@ -32,12 +32,25 @@ end
 
 shareholder_outstanding.each do |email, stories|
   from = config['from']['email']
-  # to = "#{shareholder_emails[email].name} <#{email}>"
-  # TODO This needs to handle inactive users
-  to = "jason.noble@primedia.com"
-  subject = "You need to accept Pivotal Stories"
-  
-  message = <<-END
+  if email == 'inactive'
+    to = config['inactive_email']
+    subject = config['inactive_subject'] || "You need to reassign Pivotal Stories"
+    message = <<-END
+
+#{config['from']['name']} has completed one or more of the following stories.
+
+************ ACTION ITEM *****************************************************
+
+Each of these stories needs to be reassigned to an active shareholder so that
+it can be accepted.
+
+Stories needing acceptance:
+
+  END
+  else
+    to = "#{shareholder_emails[email].name} <#{email}>"
+    subject = config['subject'] || "You need to accept Pivotal Stories"
+    message = <<-END
 
 #{config['from']['name']} has completed one or more of the stories you requested.
 Each of these stories require your acceptance in order to be complete.
@@ -45,31 +58,38 @@ Each of these stories require your acceptance in order to be complete.
 Stories needing acceptance:
 
   END
+  end
   
+  # TODO Remove this before putting anywhere
+  # to = "jason.noble@primedia.com"
   stories.each do |story|
     message << ["\t[#{story['project_name']}] #{story['story'].name}", "\tURL: #{story['story'].url}"].join("\n")
     message << "\n\n"
   end
   message <<  <<-END
-For each story, please verify the functionality you requested was implemented correctly.
+For each story, please verify the functionality you requested was implemented
+correctly.
 
-If you accept the story, please add a "sh_accepted" label by opening the story and selecting
-the "Label Actions..." dropdown, then selecting the sh_accepted label.  You can optionally add 
-comments.
+If you accept the story, please add a "sh_accepted" label by opening the story
+and selecting the "Label Actions..." dropdown, then selecting the sh_accepted
+label.  You can optionally add comments.
 
-If you do not accept the story, please add a comment to the story stating what is not implemented
-correctly.  You then need to change the status of the story from Finished/Delivered to Rejected.
+If you do not accept the story, please add a comment to the story stating what
+is not implemented correctly.  You then need to change the status of the story
+from Finished/Delivered to Rejected.
 
 If you have any questions, please let a member of the Ruby team know.
 
 Thanks,
 #{config['from']['name']}
   END
-
   puts "From: #{from}"
   puts "To: #{to}"
   puts "Subject: #{subject}"
   puts
   puts message
+  puts 
+  puts "="*80
+  puts
   #Pony.mail(:to => to, :from => from, :subject => subject, :body => message)
 end
